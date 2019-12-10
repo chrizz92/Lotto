58e564f0b31077f8d22719ffbca6e2a6b9149b1c
Christian Schadler <c.schadler@HTBLALeonding.onmicrosoft.com>
2019-12-10 20:09:18 +0100

    first

--- Lotto.ConsoleApp/Program.cs
+++ Lotto.ConsoleApp/Program.cs
@@ -6,7 +6,42 @@ namespace Lotto.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tippedNumbers = new int[6];
+
+            Console.WriteLine("Lottosimulation 6 aus 45");
+            Console.WriteLine("========================");
+
+            for(int i = 0; i < 6; i++)
+            {
+                do
+                {
+                    Console.Write("Bitte {0}. Tipp eingeben: ", i + 1);
+                    tippedNumbers[i] = Convert.ToInt32(Console.ReadLine());
+
+                    if (tippedNumbers[i] < 1 || tippedNumbers[i] > 45)
+                    {
+                        Console.WriteLine("Bitte gültige Nummer (1-45) eingeben!");
+                    }
+
+                    for (int k = 0; k < i; k++)
+                    {
+                        if (tippedNumbers[k] == tippedNumbers[i])
+                        {
+                            tippedNumbers[i]=0;
+                            Console.WriteLine("Die Nummer wurde schon gewählt!");
+                        } 
+                    }
+
+                    
+
+                } while (tippedNumbers[i] < 1 || tippedNumbers[i] > 45);
+            }
+            
+            // TESTAUSGABE
+            for(int m = 0; m < 6; m++)
+            {
+                Console.WriteLine(tippedNumbers[m]);
+            }
         }
     }
 }
